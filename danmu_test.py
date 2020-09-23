ba2b1cd5fd704c43baafa98e7011604ab97d9f41
Guoxiaocuo <54031282+Guoxiaocuo@users.noreply.github.com>
2020-09-23 22:42:18 +0800

    Add files via upload

---
+++ danmu_test.py
@@ -0,0 +1,63 @@
+# from bs4 import BeautifulSoup
+import requests
+# import os
+import json
+import pandas as pd
+# from wordcloud import WordCloud, ImageColorGenerator
+# import imageio
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import jieba
+# import collections # 词频统计库
+
+
+# 提取某一期的弹幕
+def get_danmu(num1, num2, page):
+    url = 'https://bullet-ws.hitv.com/bullet/2020/09/20/{}/{}/{}.json'
+    danmuurl = url.format(num1, num2, page)
+    res = requests.get(danmuurl)
+    res.encoding = 'utf-8'
+    jd = json.loads(res.text)
+    details = []
+    for i in range(len(jd['data']['items'])):
+        result = {}
+        result['stype'] = num2
+        result['id'] = jd['data']['items'][i]['id']
+        try:
+            result['uname'] = jd['data']['items'][i]['uname']
+        except:
+            result['uname'] = ''
+        result['content'] = jd['data']['items'][i]['content']
+        result['time'] = jd['data']['items'][i]['time']
+        try:
+            result['v2_up_count'] = jd['data']['items'][i]['v2_up_count']
+        except:
+            result['v2_up_count'] = ''
+        details.append(result)
+
+    return details
+
+
+# 输入关键信息
+def count_danmu():
+    danmu_total = []
+    num1 = input('第一个数字')
+    num2 = input('第二个数字')
+    page = int(input('输入总时长'))
+    for i in range(page):
+        danmu_total.extend(get_danmu(num1, num2, i))
+
+    return danmu_total
+
+
+def main():
+    danmu_end = []
+    # 爬前四集，所以设置了循环4次
+    for j in range(1):
+        danmu_end.extend(count_danmu())
+    df = pd.DataFrame(danmu_end)
+    df.to_excel('danmu_0619.xlsx')
+
+
+if __name__ == '__main__':
+    main()
